e4676f7de8ad1581972f8e6da1abeb44d778a6dc
Kenneth Auchenberg <kenneth@auchenberg.dk>
2014-02-01 14:21:24 +0100

    Formatting.

--- js/content.js
+++ js/content.js
@@ -6,6 +6,7 @@ chrome.extension.sendMessage({}, function(response) {
 		var n = document.getElementById('meteroverlay');
 	    n.parentNode.style.overflow = 'visible';
 	    n.parentNode.removeChild(n);
+
 	    n = document.getElementById('teaserwrapper');
 	    n.parentNode.removeChild(n);
 
